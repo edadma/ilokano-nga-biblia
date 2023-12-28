78e9c33391090463770787cde485925d34bb7818
Edward A Maxedon Sr <edadma@gmail.com>
2023-12-28 18:17:28 -0500

    code parsseBibleReference()

--- src/main/scala/io/guthub/edadma/ilokano_nga_biblia/parseBibleReference.scala
+++ src/main/scala/io/guthub/edadma/ilokano_nga_biblia/parseBibleReference.scala
@@ -1,8 +1,13 @@
 package io.guthub.edadma.ilokano_nga_biblia
 
+import scala.util.matching.Regex
+
 val bibleReference = raw"\s*((?:\d\s*)?[a-zA-Z\s*]*[a-zA-Z])\s+(\d+)\s*[.:,]\s*(\d+)\s*".r
+val repeatedSpaces = new Regex(raw"( )\1+")
 
 def parsseBibleReference(ref: String): Option[(String, Int, String)] =
   ref match
-    case bibleReference(book, chapter, verse) => Some(book.toLowerCase, chapter.toInt, verse)
+    case bibleReference(book, chapter, verse) => Some(replaceRepeatedSpaces(book).toLowerCase, chapter.toInt, verse)
     case _                                    => None
+
+def replaceRepeatedSpaces(input: String): String = repeatedSpaces.replaceAllIn(input, _ group 1)
