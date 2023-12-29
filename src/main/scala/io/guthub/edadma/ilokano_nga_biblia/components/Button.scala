6ac81bb0caeed5e51cc3f5053e3b0255f4326d62
Edward A Maxedon Sr <edadma@gmail.com>
2023-12-29 10:05:06 -0500

    Button

--- src/main/scala/io/guthub/edadma/ilokano_nga_biblia/components/Button.scala
+++ src/main/scala/io/guthub/edadma/ilokano_nga_biblia/components/Button.scala
@@ -4,7 +4,7 @@ import com.raquo.laminar.api.L.{*, given}
 
 def Button(content: Node, border: Boolean = true) =
   button(
-    `type` := "button",
+    typ := "button",
     cls := s"text-blue-700 ${if border then "border border-blue-700" else ""} hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center ${if border then "dark:border-blue-500" else ""} dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500",
     content,
   )
