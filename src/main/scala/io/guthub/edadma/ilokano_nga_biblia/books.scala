67c434059c1d13de053c8318c70fce97e930204f
Edward A Maxedon Sr <edadma@gmail.com>
2023-12-28 19:16:34 -0500

    books

---
+++ src/main/scala/io/guthub/edadma/ilokano_nga_biblia/books.scala
@@ -0,0 +1,16 @@
+package io.guthub.edadma.ilokano_nga_biblia
+
+import text.*
+
+import collection.immutable.ArraySeq
+
+val juan = ArraySeq(juan1, juan2, juan3)
+
+type Book = ArraySeq[String]
+
+val books =
+  Map(
+    "juan" -> juan,
+    "jn" -> juan,
+    "jua" -> juan,
+  )
