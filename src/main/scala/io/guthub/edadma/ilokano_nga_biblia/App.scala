3076577918b06be8c6f77010fa7832b511ff4d78
Edward A Maxedon Sr <edadma@gmail.com>
2023-12-29 19:14:47 -0500

    reactive chapter buttons

--- src/main/scala/io/guthub/edadma/ilokano_nga_biblia/App.scala
+++ src/main/scala/io/guthub/edadma/ilokano_nga_biblia/App.scala
@@ -4,10 +4,17 @@ import com.raquo.laminar.DomApi
 import com.raquo.laminar.api.L.{*, given}
 import components.*
 
-var book = juan
-var chapter = 1
+val book = Var(juan)
+val bookSignal = book.signal
+val chapter = Var(1)
+val chapterSignal = chapter.signal
 
 def App =
   Card(prevNext, foreignHtmlElement(DomApi.unsafeParseHtmlString(text.juan1)), prevNext)
 
-def prevNext = div(cls := "flex justify-between", LefttButton, RightButton)
+def prevNext =
+  div(
+    cls := "flex justify-between",
+    child <-- chapterSignal.map(ch => if ch > 1 then LefttButton else div()),
+    child <-- chapterSignal.map(ch => if ch < bookSignal.now().length then RightButton else div()),
+  )
