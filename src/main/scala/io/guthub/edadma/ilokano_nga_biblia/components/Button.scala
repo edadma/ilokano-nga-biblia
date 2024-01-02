4b63a5287f606c3a1ac0606854ddd997f223319a
Edward A Maxedon Sr <edadma@gmail.com>
2024-01-02 14:32:21 -0500

    remove Button focus styling

--- src/main/scala/io/guthub/edadma/ilokano_nga_biblia/components/Button.scala
+++ src/main/scala/io/guthub/edadma/ilokano_nga_biblia/components/Button.scala
@@ -7,7 +7,8 @@ import org.scalajs.dom.HTMLButtonElement
 def Button(modifiers: Modifier[ReactiveHtmlElement[HTMLButtonElement]]*): ReactiveHtmlElement[HTMLButtonElement] =
   button(
     typ := "button",
-    cls := s"text-blue-600 hover:bg-blue-600 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-md p-2.5 text-center inline-flex items-center dark:text-blue-400 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-400",
+    cls := s"text-blue-600 hover:bg-blue-600 hover:text-white font-medium rounded-full text-md p-2.5 text-center inline-flex items-center dark:text-blue-400 dark:hover:text-white dark:hover:bg-blue-400",
+    // focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800
 //    cls.toggle("border border-blue-600 dark:border-blue-400") := border,
     modifiers,
   )
