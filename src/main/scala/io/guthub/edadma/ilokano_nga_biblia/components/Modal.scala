67077030a6743a476178c864d4d7826cc6d7101e
Edward A Maxedon Sr <edadma@gmail.com>
2024-01-12 15:14:32 -0500

    fix modal

--- src/main/scala/io/guthub/edadma/ilokano_nga_biblia/components/Modal.scala
+++ src/main/scala/io/guthub/edadma/ilokano_nga_biblia/components/Modal.scala
@@ -8,10 +8,10 @@ def Modal(
     modifiers: Modifier[ReactiveHtmlElement[HTMLDivElement]]*,
 )(show: Var[Boolean], title: String, modalCls: String = ""): ReactiveHtmlElement[HTMLDivElement] =
   div(
-    cls := s"overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full $modalCls",
+    cls := "overflow-y-auto overflow-x-hidden fixed bg-black bg-opacity-50 top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full",
     cls.toggle("hidden") <-- show.signal.map(!_),
     div(
-      cls := "relative p-4 w-full max-w-2xl max-h-full",
+      cls := s"relative p-4 w-full $modalCls max-w-2xl max-h-full",
       div(
         cls := "relative bg-white rounded-lg shadow dark:bg-gray-700",
         div(
