744c7401a8135895894ccc724a4214a5e626ccc8
Edward A Maxedon Sr <edadma@gmail.com>
2024-01-14 13:22:09 -0500

    add search icon

--- src/main/scala/io/guthub/edadma/ilokano_nga_biblia/components/SVG.scala
+++ src/main/scala/io/guthub/edadma/ilokano_nga_biblia/components/SVG.scala
@@ -20,6 +20,25 @@ object SVG:
       ),
     )
 
+  def search: ReactiveSvgElement[SVGSVGElement] =
+    svg(
+      cls := "w-[16px] h-[16px]",
+      xmlns := "http://www.w3.org/2000/svg",
+      fill := "none",
+      viewBox := "0 0 20 20",
+      path(
+        stroke := "currentColor",
+        strokeLineCap := "round",
+        strokeLineJoin := "round",
+        strokeWidth := "2",
+        d := "m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z",
+      ),
+    )
+
+  //    <svg class="w-[16px] h-[16px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+//      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+//    </svg>
+
   def about: ReactiveSvgElement[SVGSVGElement] =
     svg(
       cls := "w-[16px] h-[16px]",
