5fd5ee57aeb18e608510263fb308c0427102dce9
Edward A Maxedon Sr <edadma@gmail.com>
2024-02-18 13:15:56 -0500

    add filemon

---
+++ src/main/scala/io/guthub/edadma/ilokano_nga_biblia/text/filemon/book.scala
@@ -0,0 +1,5 @@
+package io.guthub.edadma.ilokano_nga_biblia.text.filemon
+
+import collection.immutable.ArraySeq
+
+val book = ArraySeq((filemon_1, 25))
---
+++ src/main/scala/io/guthub/edadma/ilokano_nga_biblia/text/filemon/filemon_1.scala
@@ -0,0 +1,36 @@
+package io.guthub.edadma.ilokano_nga_biblia.text.filemon
+
+val filemon_1 =
+"""<div class="prose prose-h1:font-fondamento prose-h1:font-normal prose-h1:text-gray-600 dark:prose-h1:text-gray-400 prose-h2:text-gray-600 dark:prose-h2:text-gray-400 prose-h3:text-gray-600 dark:prose-h3:text-gray-400 $size prose-p:text-gray-600 dark:prose-p:text-gray-400 prose-p:m-0 prose-p:leading-normal">
+<h1>Surat ni Pablo kenni Filemon</h1>
+<h3>Kablaaw</h3>
+<p class=""><sup class="mr-px" id="1">1</sup>Ni Pablo, maysa a balud gapu ken Cristo Jesus, ken ni Timoteo a kabsattayo.</p>
+<p class="indent-8">Ken ni Filemon a dungdungnguenmi a katrabahuan
+<sup class="mr-px" id="2">2</sup>ken ni Afia a kabsatmi ken ni Arquipo a padami a soldado, ken ti iglesia iti balayyo.</p>
+<p class="indent-8"><sup class="mr-px" id="3">3</sup>Parabur ken talna ti Dios Amatayo ken ni Apo Jesu-Kristo.</p>
+<h3>Ti Ayat ken Pammati ni Filemon</h3>
+<p class=""><sup class="mr-px" id="4">4</sup>Kanayon nga agyamanak iti Diosko no laglagipenka kadagiti kararagko,
+<sup class="mr-px" id="5">5</sup>agsipud ta mangngegko ti ayatmo ken ti pammatim ken Apo Jesus ken kadagiti amin a sasanto,
+<sup class="mr-px" id="6">6</sup>ket ikararagko nga agbalin koma ti panangiranudmo iti pammatim epektibo para iti naan-anay a pannakaammo iti tunggal naimbag a banag nga adda kadatayo gapu ken Cristo.
+<sup class="mr-px" id="7">7</sup>Ta nakagun-odak iti adu a rag-o ken liwliwa manipud iti ayatmo, kabsatko, agsipud ta nabang-aran dagiti puso dagiti sasanto babaen kenka.</p>
+<h3>Ti Pakaasi ni Pablo ken Onesimo</h3>
+<p class=""><sup class="mr-px" id="8">8</sup>Maitunos iti dayta, nupay naturedak ken Kristo a mangibilin kadakayo nga aramidenyo ti makalikaguman,
+<sup class="mr-px" id="9">9</sup>kaskasdi gapu iti ayat kaykayatko ti umapela kadakayo—siak, ni Pablo, maysa a lakay ken ita baludak met gapu ken Kristo Jesus—
+<sup class="mr-px" id="10">10</sup>umapelaak kenka gapu iti anakko, ni Onesimo, a nagbalinak nga ama iti pannakaibaludko.
+<sup class="mr-px" id="11">11</sup>(Sigud nga awan serserbina kadakayo, ngem ita, pudno a makagunggona kadakayo ken kaniak.)
+<sup class="mr-px" id="12">12</sup>Isublik kadakayo, nga ibaonko ti mismo a pusok.
+<sup class="mr-px" id="13">13</sup>Maragsakanak koma a mangtaginayon kenkuana, tapno agserbi kaniak iti biangmo bayat ti pannakaibaludko maipaay iti naimbag a damag,
+<sup class="mr-px" id="14">14</sup>ngem kaykayatko nga awan ti aniaman nga aramiden no awan ti pammalubosmo tapno ti kinaimbagmo ket saan a gapu iti pannakapilit no di ket iti bukodmo a pagayatan.
+<sup class="mr-px" id="15">15</sup>Ta daytoy nalabit ti makagapu a naisina kadakayo iti apagbiit, tapno maisubliyo isuna iti agnanayon,
+<sup class="mr-px" id="16">16</sup>saanen nga adipen no di ket ad-adda ngem adipen, kas maysa a dungdungnguen a kabsat—nangnangruna kaniak, ngem anian a kaadu ad-adu pay kadakayo, agpadpada iti lasag ken iti Apo.</p>
+<p class="indent-8"><sup class="mr-px" id="17">17</sup>Isu a no ibilangdak a kaduayo, awatenyo a kas iti panangawatyo kaniak.
+<sup class="mr-px" id="18">18</sup>No nakabasol kenka, wenno adda utangna kenka, singirem dayta iti kuentak.
+<sup class="mr-px" id="19">19</sup>Siak, ni Pablo, isuratko daytoy babaen ti bukodko nga ima: Supatak dayta—tapno awan ti maibagam maipapan iti utangmo kaniak uray ti bukodmo a bagi.
+<sup class="mr-px" id="20">20</sup>Wen, kabsat, kayatko ti sumagmamano a pagimbagan manipud kenka iti Apo. Pabaroem ti pusok ken Cristo.</p>
+<p class="indent-8"><sup class="mr-px" id="21">21</sup>Gapu ta agtalekak iti panagtulnogyo, agsuratak kadakayo, ta ammok nga ad-adu pay ti maaramidam ngem iti ibagbagak.
+<sup class="mr-px" id="22">22</sup>Maigiddato iti dayta, mangisaganakayo iti siled dagiti sangaili para kaniak, ta namnamaek a babaen kadagiti kararagyo maitedak kadakayo a siaasi.</p>
+<h3>Maudi a Kablaaw</h3>
+<p class=""><sup class="mr-px" id="23">23</sup>Kablaaw kadakayo ni Epafras, padak a balud ken Kristo Jesus,
+<sup class="mr-px" id="24">24</sup>kasta met da Marcos, Aristarco, Demas, ken Lucas, katrabahuak.</p>
+<p class="indent-8"><sup class="mr-px" id="25">25</sup>Ti parabur ni Apo Jesu-Kristo adda koma iti espirituyo.</p>
+</div>"""
