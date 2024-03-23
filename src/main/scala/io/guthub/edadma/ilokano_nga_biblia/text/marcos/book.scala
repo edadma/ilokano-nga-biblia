df969c66f109171774238f03077cd9b7f20a370a
Edward A Maxedon Sr <edadma@gmail.com>
2024-03-23 15:42:39 -0400

    add marcos

--- src/main/scala/io/guthub/edadma/ilokano_nga_biblia/text/marcos/book.scala
+++ src/main/scala/io/guthub/edadma/ilokano_nga_biblia/text/marcos/book.scala
@@ -2,4 +2,4 @@ package io.guthub.edadma.ilokano_nga_biblia.text.marcos
 
 import collection.immutable.ArraySeq
 
-val book = ArraySeq((marcos_1, 45), (marcos_2, 28), (marcos_3, 35), (marcos_4, 41), (marcos_5, 43), (marcos_6, 56), (marcos_7, 17), (marcos_8, 38), (marcos_9, 43), (marcos_10, 52), (marcos_11, 44), (marcos_12, 37), (marcos_13, 72), (marcos_14, 30), (marcos_15, 20))
+val book = ArraySeq((marcos_1, 45), (marcos_2, 28), (marcos_3, 35), (marcos_4, 41), (marcos_5, 43), (marcos_6, 56), (marcos_7, 17), (marcos_8, 38), (marcos_9, 43), (marcos_10, 52), (marcos_11, 26), (marcos_12, 44), (marcos_13, 37), (marcos_14, 72), (marcos_15, 30), (marcos_16, 20))
---
+++ src/main/scala/io/guthub/edadma/ilokano_nga_biblia/text/marcos/marcos_11.scala
@@ -0,0 +1,37 @@
+package io.guthub.edadma.ilokano_nga_biblia.text.marcos
+
+val marcos_11 =
+"""<div class="prose prose-h1:font-quintessential prose-h1:font-normal prose-h1:text-gray-600 dark:prose-h1:text-gray-400 prose-h2:text-gray-600 dark:prose-h2:text-gray-400 prose-h3:text-gray-600 dark:prose-h3:text-gray-400 $size prose-p:text-gray-600 dark:prose-p:text-gray-400 prose-p:m-0 prose-p:leading-normal">
+<h2>Kapitulo 11</h2>
+<h3>Ti Naballigi a Panagserrek</h3>
+<p class=""><sup class="mr-px" id="1">1</sup>Ita idi immasidegda idiay Jerusalem, idiay Betfage ken Betania, idiay Bantay dagiti Olibo, nangibaon ni Jesus iti dua nga adalanna
+<sup class="mr-px" id="2">2</sup>ket kinunana kadakuada, “Inkayo iti purok iti sanguananyo, ket dagus a sumrekkayo it masarakamto ti maysa nga urbon nga urbon a naigalut, nga awan pay ti nagtugaw. Uk-ibukbok ken iyeg.
+<sup class="mr-px" id="3">3</sup>No adda agkuna kadakayo, ‘Apay nga ar-aramidenyo daytoy?’ ibagayo, ‘Kasapulan ti Apo ket isublina a dagus ditoy.’”
+<sup class="mr-px" id="4">4</sup>Ket napanda ket nakasarakda iti maysa nga urbon nga asno a naigalut iti maysa a ruangan iti ruar iti kalsada, ket inukradda dayta.
+<sup class="mr-px" id="5">5</sup>Kinuna kadakuada ti dadduma nga agtaktakder, “Ania ti ar-aramidenyo a manglukat iti urbon nga urbon?”
+<sup class="mr-px" id="6">6</sup>Ket imbagada kadakuada ti imbaga ni Jesus, ket pinalubosanda ida.
+<sup class="mr-px" id="7">7</sup>Ket inyegda ti urbon nga asno ken ni Jesus ket inwarasda dagiti kabalda, ket isu nagtugaw iti dayta.
+<sup class="mr-px" id="8">8</sup>Adu ti nangiwaras kadagiti kabalda iti dalan, ken dadduma ti nangiwaras kadagiti nabulong a sanga a pinutedda manipud iti talon.
+<sup class="mr-px" id="9">9</sup>Ket dagiti immun-una ken dagiti simmaruno ket agpukpukkawda, “Hosanna! Bendito ti umay iti nagan ti Apo!
+<sup class="mr-px" id="10">10</sup>Bendito ti umay a pagarian ni amatayo a David! Hosanna iti kangatuan!”</p>
+<p class="indent-8"><sup class="mr-px" id="11">11</sup>Simrek iti Jerusalem ket simrek iti templo. Ket idi kinitana amin, ta naladaw, rimmuar idiay Betania a kaduana dagiti sangapulo ket dua.</p>
+<h3>Ilunod ni Jesus ti Kaykayo a Higos</h3>
+<p class=""><sup class="mr-px" id="12">12</sup>Iti simmaganad nga aldaw, idi naggapuda idiay Betania, mabisin isuna.
+<sup class="mr-px" id="13">13</sup>Ket idi nakitana iti adayo ti maysa a kayo nga igos a nabulong, napanna kitaen no adda masarakanna iti dayta. Idi dimteng iti dayta, awan ti nasarakanna no di dagiti bulbulong, ta saan a panawen ti higos.
+<sup class="mr-px" id="14">14</sup>Ket kinunana iti daytoy, “Awan koma ti mangan manen iti bunga manipud kenka.” Ket nangngeg dayta dagiti adalanna.</p>
+<h3>Dalusan ni Jesus ti Templo</h3>
+<p class=""><sup class="mr-px" id="15">15</sup>Ket dimtengda idiay Jerusalem. Ket simrek iti templo ket rinugianna a papanawen dagiti aglaklako ken dagiti gumatang iti templo, ket binaliktadna dagiti lamisaan dagiti agsuksukat iti kuarta ken dagiti tugaw dagiti aglaklako iti kalapati.
+<sup class="mr-px" id="16">16</sup>Ket saanna nga palubosan ti asinoman a mangawit iti aniaman a banag iti templo.
+<sup class="mr-px" id="17">17</sup>Isursurona ida ket kinunana kadakuada, “Saan kadi a naisurat, ‘Ti balayko maawaganto iti balay ti kararag para kadagiti amin a nasion’? Ngem pinagbalinmo a kueba dagiti mannanakaw.”
+<sup class="mr-px" id="18">18</sup>Nangngeg dayta dagiti panguluen a papadi ken dagiti eskriba ket agsapsapulda iti wagas a mangdadael kenkuana, ta nagbutengda kenkuana, agsipud ta nasdaaw ti amin a tattao iti pannursurona.
+<sup class="mr-px" id="19">19</sup>Ket idi dumteng ti rabii rimmuarda iti siudad.</p>
+<h3>Ti Leksion manipud iti Nalaylay a Kaykayo ti Higos</h3>
+<p class=""><sup class="mr-px" id="20">20</sup>Bayat a lumabasda iti agsapa, nakitada ti kayo a higos a nalaylay agingga kadagiti ramutna.
+<sup class="mr-px" id="21">21</sup>Nalagip ni Pedro ket kinunana kenkuana, “Maestro, adtoy! Nalaylay ti kayo nga igos nga inlunodmo.”
+<sup class="mr-px" id="22">22</sup>Simmungbat ni Jesus kadakuada, “Mamatikayo iti Dios.
+<sup class="mr-px" id="23">23</sup>Pudno, kunak kadakayo, siasinoman a mangibaga iti daytoy a bantay, ‘Maalakayo ket maitapuakkayo iti baybay,’ ket saan nga agduadua iti pusona, no di ket mamati a mapasamakto ti ibagbagana, maaramidto dayta para kenkuana .
+<sup class="mr-px" id="24">24</sup>Ngarud ibagak kadakayo, aniaman a dawatenyo iti kararag, patienyo nga inawat dayta, ket kukuayonto.
+<sup class="mr-px" id="25">25</sup>Ket tunggal agtakderkayo nga agkararag, pakawanenyo, no adda aniaman a maibusor iti asinoman, tapno mapakawannaka met ni Amayo nga adda sadi langit kadagiti basolyo.”</p>
+<h3>Nakarit ti Autoridad ni Jesus</h3>
+<p class=""><sup class="mr-px" id="27">27</sup>Ket immayda manen idiay Jerusalem. Ket bayat a magmagna iti templo, immay kenkuana dagiti panguluen a papadi ken dagiti eskriba ken dagiti panglakayen, 28 ket kinunada kenkuana, “Babaen iti ania nga autoridad nga ar-aramidem dagitoy a banag, wenno siasino ti nangted kenka iti daytoy nga autoridad a mangaramid kadagitoy?” 29 Kinuna ni Jesus kadakuada, “Saludsodek kadakayo ti maysa a saludsod; sungbatannak, ket ibagak kadakayo no ania ti turay nga aramidek dagitoy a banbanag. 30 Naggapu kadi idiay langit wenno naggapu iti tao ti bautismo ni Juan? Sungbatannak.” 31 Ket nagsasaritaanda dayta iti maysa ken maysa, a kunkunada, “No kunaentayo, ‘Manipud langit,’ kunaennanto, ‘Apay ngarud a dikay pinati?’ 32 Ngem kunaentayo kadi, ‘Manipud iti tao’?” mabuteng kadagiti tattao, ta impagarupda amin a ni Juan ket talaga a propeta. 33 Isu a simmungbatda ken Jesus, “Saanmi nga ammo.” Ket kinuna ni Jesus kadakuada, “Diakto met ibaga kadakayo no ania ti turay nga aramidek dagitoy.”</p>
+</div>"""
